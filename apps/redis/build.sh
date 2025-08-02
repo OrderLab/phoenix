dfbafdad9bf24d8afd79f14df8760f5f9a07b016
robertking <me@robertking.dev>
2025-08-01 19:41:40 -0700

    Update Redis build

--- apps/redis/build.sh
+++ apps/redis/build.sh
@@ -3,24 +3,31 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR 
 
-git clone git@github.com:OrderLab/phoenix-redis.git code
+set -x
+
+git clone https://github.com/OrderLab/phoenix-redis.git code
 cd code
 make distclean
+
+function fail {
+	status=$?
+	echo "Compilation Failed!"
+	exit $status
+}
 
 function build {
 	commit=$1
 	target=$2
 	patch=$3
 
-	git reset --hard && git checkout -f $commit || exit 1
-	[ ! -z "$patch" ] && git apply $SCRIPT_DIR/$patch || exit 1
-	make USE_JEMALLOC=no -j$(nproc) || exit 1
-	make install PREFIX=$SCRIPT_DIR/$target || exit 1
-	make distclean || exit 1
+	git reset --hard && git checkout -f $commit || fail
+	[ ! -z "$patch" ] && (git apply $SCRIPT_DIR/$patch || fail)
+	make install PREFIX=$SCRIPT_DIR/$target USE_JEMALLOC=no -j$(nproc) || fail
+	make distclean || fail
 	git checkout -- .
 }
 
-build ff28130 rel-phx-72 phx-redis.patch || exit $?
-build 4defa86 rel-orig-72 vanilla-redis.patch || exit $?
-build cc3e659 rel-phx-7445 || exit $?
-build 351252a rel-orig-7445 vanilla-redis-7445.patch || exit $?
+build 0ee2046 rel-phx-72 phx-redis.patch || fail
+build 4defa86 rel-orig-72 vanilla-redis.patch || fail
+build 50e7443 rel-phx-7445 || fail
+build 351252a rel-orig-7445 vanilla-redis-7445.patch || fail
