a46ee5b8cd425aa33c6af5f294cbd40224412349
robertking <me@robertking.dev>
2025-08-05 21:30:25 -0400

    Update Redis script and result folder naming; but original Redis crashes on coffeelake...

--- experiments/README.md
+++ experiments/README.md
@@ -5,12 +5,12 @@ We provide scripts for the following list of experiments mentioned in the paper.
 ## Recovery Time (Figure 9)
 
 For each system and each recovery mechanism, `cd` into each system's folder,
-run `./run<mechanism>.sh` with an experiment name, case number, and the round
-of run. For example, to run Redis case 761 using Builtin for 5 times, run the
-following command in `redis` folder:
+run `./run<mechanism>.sh`, with case number, and the round of run. For example,
+to run Redis case 761 using Builtin for 5 times, run the following command in
+`redis` folder:
 
 ```bash
-for i in {1..5}; do ./runbuiltin.sh sosp 761 $i; done
+for i in {1..5}; do ./runbuiltin.sh 761 $i; done
 ```
 
-This will generate a `result-builtin-sosp` results folder.
+This will generate a `result-761` results folder, containing `builtin-1-*` log files.
--- experiments/redis/runvanilla.sh
+++ experiments/redis/runvanilla.sh
@@ -3,30 +3,68 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
-if [ "$#" -ne 3 ]; then
-	echo "Usage: $0 <expgroup> <case> <idx> [killtime]"
-	exit 1
+source /usr/share/modules/init/bash
+
+MODE=vanilla
+
+EXP=
+CASE=
+IDX=
+SLEEP_TIME=120
+
+cmdname=$0
+
+function usage {
+    code=${1:-0}
+
+    echo "Usage: $cmdname <case> <idx> [-g|--expgroup=<name>] [-k|--killtime=120]
+    Creates an output folder named result-<case>.
+    --exp-group adds a -<name> suffix to the output folder."
+
+    exit $code
+}
+
+function fail {
+    echo "$1"
+    exit 1
+}
+
+posargidx=0
+while (( $# )); do
+    case "$1" in
+        -g=*|--expgroup=*)
+            EXP="-${1#*=}";;
+        -k=*|--killtime=*)
+            SLEEP_TIME="${1#*=}";;
+        -h|--help)
+            usage;;
+        *)
+            case $posargidx in
+            0)  CASE="$1";;
+            1)  IDX="$1";;
+            *)  usage 1;;
+            esac
+            posargidx=$((posargidx+1));;
+    esac
+    shift
+done
+
+if (( "$posargidx" != 2 )); then
+    usage 1
 fi
 
 set -x
 
-EXP=${1}
-CASE=${2}
-IDX=${3}
-SLEEP_TIME=${4:-120}
-
-FOLDER="result-vanilla-$EXP"
-
-mkdir -p "$FOLDER"
+FOLDER="result-$CASE$EXP"
 
 TRIGGER="trigger-$CASE.sh"
-LOADLOG="$FOLDER/$CASE-$IDX-load.log"
-SERVERLOG="$FOLDER/$CASE-$IDX-server.log"
-TRIGGERLOG="$FOLDER/$CASE-$IDX-trigger.log"
-WKLOG="$FOLDER/$CASE-$IDX.log"
+LOADLOG="$FOLDER/$MODE-$IDX-load.log"
+SERVERLOG="$FOLDER/$MODE-$IDX-server.log"
+TRIGGERLOG="$FOLDER/$MODE-$IDX-trigger.log"
+WKLOG="$FOLDER/$MODE-$IDX-ycsb.log"
 
 if ! [ -f "$TRIGGER" ]; then
-	echo "Cannot find repro script"
+	echo "Cannot find script $TRIGGER"
 	exit 1
 fi
 case "$CASE" in
@@ -35,10 +73,14 @@ case "$CASE" in
 	(*) echo "Unknown case $CASE" >&2; exit 1;;
 esac
 
+mkdir -p "$FOLDER" || fail "Cannot create folder $FOLDER"
+
 set +x
-module load ycsb || exit 1
-module load redis/"$SYS" || exit 1
+module load ycsb || fail "Cannot load ycsb"
+module load redis/"$SYS" || fail "Cannot load redis/$SYS"
 module list
+command -v ycsb || fail "Command not found: ycsb"
+command -v redis-server || fail "Command not found: redis-server"
 set -x
 
 function runserver {
